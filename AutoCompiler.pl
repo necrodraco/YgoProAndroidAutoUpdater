a3fe7cbbdf78fd7cd3fd0a40475834a682355836
NecroDraco <ygohameln@gmail.com>
2017-04-17 22:35:15 +0200

    Unworkable, add bunch of Code

--- AutoCompiler.pl
+++ AutoCompiler.pl
@@ -10,3 +10,249 @@ use Archive::Zip;
 use Archive::Zip qw(:ERROR_CODES); 
 use Data::Dumper; 
 no warnings 'experimental::smartmatch';
+
+##DON'T Change anything following
+#Parameters without change: 
+my $command; 
+my $image;
+my @pathsWithCDB = (); 
+my @imageList = (); 
+my $filename = "template.properties";#"settings.properties";
+my $values = ();
+my $ref; 
+##Methods: 
+sub readMethods(){
+	open(my $file, "<", $filename) or die "can't open ".$filename;
+		while (my $row = <$file>) {
+				if(!($row =~ "#")){
+					my @items = split(/=/,$row);
+					$items[1] =~ s/\n//g;
+					$values->{$items[0]} = $items[1]; 
+				}
+			}
+	close($file);
+}
+
+###############################################
+##Deprecated Methods
+
+sub returnAllDatabases(){
+	my $F = $File::Find::name; 
+
+	if($F =~ /\.cdb$/){
+		push(@pathsWithCDB, "$F");#"\"$F\"");
+	}
+}
+sub returnAllImages(){
+	my $F = $File::Find::name; 
+	if($F =~ /\.jpg$/){
+		push(@imageList, "$F");
+	}
+}
+sub prepareParams($){
+	my $pathsRef = shift;
+	my @paths = @{$pathsRef};
+	my @lPath = (); 
+	do{
+		my $path = shift @paths;
+		#if(1 > -M $path){
+			if(!($path =~ m/\/language\//)){
+				if($path =~ m/\/cards-tf.cdb/){
+					unshift(@lPath, $path);
+				}else{
+					if($path =~ m/\/live2/){
+						push(@lPath, $path);
+					}else{
+						if(@paths ~~ m/\/live2/){
+							push(@paths, $path);
+						}else{
+							if($path =~ m/\/liveanime\//){
+								push(@lPath, $path);
+							}else{
+								if(@paths ~~ m/\/liveanime\//){
+									push(@paths, $path);
+								}else{
+									if($path =~ m/\/expansions\//){
+										push(@lPath, $path);
+									}else{
+										if(@paths ~~ m/\/expansions\//){
+											push(@paths, $path);
+										}else{
+											push(@lPath, $path);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		#}
+	}while(@paths);
+	return \@lPath;
+}
+sub doSqlLiteMerge($){
+	$ref = shift; 
+	my @list = @{$ref};
+	my $dest = $values->{pathToApkFolder}."/assets/cards.cdb";
+	my $src = shift @list; 
+	copy($src, $dest) or die "Copy failed: $!";
+	
+	my $dbargs = {AutoCommit => 1, PrintError => 1};
+	my $dbh = DBI->connect("dbi:SQLite:dbname=$dest", "", "", $dbargs);
+
+	foreach my $cdbFile(@list){
+		doSqlQuery($dbh, "attach '".$cdbFile."' as toMerge");
+		doSqlQuery($dbh, "insert or ignore into datas select * from toMerge.datas");
+		doSqlQuery($dbh, "insert or ignore into texts select * from toMerge.texts");
+		doSqlQuery($dbh, "detach toMerge");
+	}	
+	$dbh->disconnect();
+}
+sub doSqlQuery($){
+	my $dbh = shift; 
+	my $statement = shift; 
+	$dbh->do($statement)or die "$DBI::errstr\n"; 
+}
+sub doCommand($){
+	my $command = shift; 
+	system "$command";
+}
+sub doGitPull($){
+	my $refarg = shift; 
+	my @arg = @{$refarg};
+	foreach my $argument(@arg){
+		$command = "cd ".$values->{pathToYgopro}.$argument." && git pull";
+		system "$command";
+	}
+}
+sub doPic($){
+	$ref = shift; 
+	my @list = @{$ref};
+	foreach my $file (@list){
+		my @items = split(/pics/, $file);
+		my $src = $items[0]."/pics".$items[1]; 
+		my $dest = $values->{imageFolder};
+
+		$image = new Image::Magick; 
+		$image->Read($src);
+		$image->Set(quality=>'90');
+		$image->Strip();
+		$image->Write($dest.$items[1]);
+	}
+}
+sub doSymlink(){
+	##Remove all Symlinks for the Script Files
+	doCommand("cd ".$values->{pathToApkScriptFolder}." && rm *.lua");
+
+	##Create all the Symlinks for the Script-Files
+	doCommand("java -jar Symlinker.jar "
+			.$values->{pathToApkScriptFolder}." "
+			.$values->{manualFolder}." "
+			.$values->{oldScriptFOlder}." "
+			.$values->{live2017ScriptFolder}." "
+			.$values->{liveAnimeScriptFolder}." "
+			.$values->{ygoproScriptFolder}
+		);
+}
+sub doApk($){
+	$ref = shift; 
+	my %ai = %{$ref};
+	foreach my $key(keys %ai){
+		doCommand("cd ".$values->{pathToApkFolder}."/assets/ai && rm full.lua && ln -s ".$values->{pathToAIs}."".$ai{$key}." full.lua");
+
+		##create the new APK
+		doCommand("apktool b -o ".$values->{pathToApkFolder}.$key.".apk ".$values->{pathToApkFolder});
+
+		##sign it
+		doCommand("apksign ".$values->{pathToApkFolder}.$key.".apk"); 
+
+		##rename
+		doCommand("rm ".$values->{pathToApkFolder}.$key.".apk && mv ".$values->{pathToApkFolder}.$key.".s.apk ".$values->{pathToApkFolder}.$key.".apk"); 
+	}
+}
+sub saveInArchive($){
+	$ref = shift; 
+	my @reference = @{$ref};
+	my $archiveName = shift @reference; 
+	my $items = shift @reference; 
+	my $archive = Archive::Zip->new();
+	foreach my $key(keys %$items){
+		if($key =~ /folder/){
+			$archive->addTree($items->{$key}."/", $items->{$key});#$pathToAddingFolder, $folderItems{$key});
+		}else{
+			$archive->addFile($items->{$key}) or die "Error during Add File";
+		}
+	}
+	$archive->writeToFileNamed($archiveName) == AZ_OK or die "Error during writing to Archive ";
+}
+sub doImages(){
+	##Create the pic-items
+	find({ wanted => \&returnAllImages, no_chdir=>1}, $values->{pathToYgopro}.$values->{liveImages});#."/pics");
+	doPic(\@imageList);
+
+	##Do Archiving Part 1
+	my %pathImage = ("folder1"=>"pics");
+	my @args = (
+		"main.4.co.ygopro.ygoproandroid.obb", 
+		\%pathImage
+		);
+	saveInArchive(\@args);
+
+	##DO Archiving Part 2
+	my %pathArchiveFolderOnly = (
+		"folder1" => "ai", 
+		"file1" => "patch.48.co.ygopro.ygoproandroid.obb",
+		"file2" => "main.4.co.ygopro.ygoproandroid.obb"
+		);
+	@args = (
+		"pics_normal.zip", 
+		\%pathArchiveFolderOnly
+		);
+	saveInArchive(\@args);
+}
+sub doRest(){
+	##Do all the Script Files
+	doSymlink();
+
+	##Do all the Sqlite-Shit
+	find({ wanted => \&returnAllDatabases, no_chdir=>1}, $values->{pathToYgopro});
+	doSqlLiteMerge(prepareParams(\@pathsWithCDB));
+
+	##Do all to get the APK
+	my %ais = (
+			"_simple" => "full_simple.lua",
+			"_experienced" => "full_experienced.lua"
+		);
+	doApk(\%ais);
+
+	##upload
+	##TODO
+}
+
+##############################################
+##End of Deprecated Methods
+
+##Actions
+print "started\n";
+
+#Read all the Properties from settings.properties and save them in $values
+readMethods();
+
+if($values->{testing} eq "1"){
+	print Dumper $values;
+}
+
+#Deprecated Actions
+if($values->{testing} eq "1"){
+	##Do all Git Pulls
+	my @list = ($values->{liveImages},$values->{live2017},$values->{liveanime});
+	doGitPull(\@list);
+
+	##Do All the Image-Things and Archiving Things
+	#doImages();
+
+	#doRest();
+}
+
+print "finished\n";
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # YgoProAndroidAutoUpdater
 Script which allows automatically Update YgoProAndroid. In Developing
+
+#Tested Only on Linux / Ubuntu
+
+#On Windows you could get problems with e.g. the Symlinker
+
+#Need to be installed: 
+perl #test with perl --version
+java #test with java -version
+perl sqlite3 Module # install on Ubuntu by sudo apt-get install libdbd-sqlite3-perl
+ImageMagic #install on Ubuntu by sudo apt-get install imagemagick imagemagick-doc
+perl MooX Module
+Git LTS #Look for documentation #Only needed when Fill out the Upload-Options
+Apktool #Can be found here: https://ibotpeaches.github.io/Apktool/
+Apksign #Can be found here: https://github.com/appium/sign
+YGOPRO Liveimages #Can be found here:  run "git clone https://github.com/Ygoproco/Live-images.git" in /ygopro of your desktopClient
+
+#How To use: 
+1. Rename/copy the "template.properties" to "settings.properties"
+2. Fill in all the Lines
+3. Run perl AutoCompiler.pl
