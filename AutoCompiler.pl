6f1b2e1cad085e6fa1325fb045f4594f07e3afcc
Jan_Schule <fiae15h.brandt@mmbbs.eduplaza.de>
2017-04-18 15:39:10 +0200

    little additions

--- AutoCompiler.pl
+++ AutoCompiler.pl
@@ -160,46 +160,6 @@ sub doApk($){
 		doCommand("rm ".$values->{pathToApkFolder}.$key.".apk && mv ".$values->{pathToApkFolder}.$key.".s.apk ".$values->{pathToApkFolder}.$key.".apk"); 
 	}
 }
-sub saveInArchive($){
-	$ref = shift; 
-	my @reference = @{$ref};
-	my $archiveName = shift @reference; 
-	my $items = shift @reference; 
-	my $archive = Archive::Zip->new();
-	foreach my $key(keys %$items){
-		if($key =~ /folder/){
-			$archive->addTree($items->{$key}."/", $items->{$key});#$pathToAddingFolder, $folderItems{$key});
-		}else{
-			$archive->addFile($items->{$key}) or die "Error during Add File";
-		}
-	}
-	$archive->writeToFileNamed($archiveName) == AZ_OK or die "Error during writing to Archive ";
-}
-sub doImages(){
-	##Create the pic-items
-	find({ wanted => \&returnAllImages, no_chdir=>1}, $values->{pathToYgopro}.$values->{liveImages});#."/pics");
-	doPic(\@imageList);
-
-	##Do Archiving Part 1
-	my %pathImage = ("folder1"=>"pics");
-	my @args = (
-		"main.4.co.ygopro.ygoproandroid.obb", 
-		\%pathImage
-		);
-	saveInArchive(\@args);
-
-	##DO Archiving Part 2
-	my %pathArchiveFolderOnly = (
-		"folder1" => "ai", 
-		"file1" => "patch.48.co.ygopro.ygoproandroid.obb",
-		"file2" => "main.4.co.ygopro.ygoproandroid.obb"
-		);
-	@args = (
-		"pics_normal.zip", 
-		\%pathArchiveFolderOnly
-		);
-	saveInArchive(\@args);
-}
 sub doRest(){
 	##Do all the Script Files
 	doSymlink();
@@ -249,7 +209,7 @@ print "Updated Local Instance of YgoPro Client completely\n";
 if($values->{testing} eq "1"){
 
 	##Do All the Image-Things and Archiving Things
-	#doImages();
+	doImages();
 
 	#doRest();
 }
--- ImageWorker.pm
+++ ImageWorker.pm
@@ -2,8 +2,9 @@
 use strict;
 use warnings; 
 use Image::Magick; 
-#require "AutoCompiler.pl";
 our ($values, $image);
+
+##Contains all the Methods for manipulating the Images and OBB-Archives
 sub doPic($){
 	my $ref = shift; 
 	my @list = @{$ref};
@@ -18,6 +19,48 @@ sub doPic($){
 		$image->Strip();
 		$image->Write($dest.$items[1]);
 	}
+}
+
+sub saveInArchive($){
+	$ref = shift; 
+	my @reference = @{$ref};
+	my $archiveName = shift @reference; 
+	my $items = shift @reference; 
+	my $archive = Archive::Zip->new();
+	foreach my $key(keys %$items){
+		if($key =~ /folder/){
+			$archive->addTree($items->{$key}."/", $items->{$key});#$pathToAddingFolder, $folderItems{$key});
+		}else{
+			$archive->addFile($items->{$key}) or die "Error during Add File";
+		}
+	}
+	$archive->writeToFileNamed($archiveName) == AZ_OK or die "Error during writing to Archive ";
+}
+
+sub doImages(){
+	##Create the pic-items
+	find({ wanted => \&returnAllImages, no_chdir=>1}, $values->{pathToYgopro}.$values->{liveImages});#."/pics");
+	doPic(\@imageList);
+
+	##Do Archiving Part 1
+	my %pathImage = ("folder1"=>"pics");
+	my @args = (
+		"main.4.co.ygopro.ygoproandroid.obb", 
+		\%pathImage
+		);
+	saveInArchive(\@args);
+
+	##DO Archiving Part 2
+	my %pathArchiveFolderOnly = (
+		"folder1" => "ai", 
+		"file1" => "patch.48.co.ygopro.ygoproandroid.obb",
+		"file2" => "main.4.co.ygopro.ygoproandroid.obb"
+		);
+	@args = (
+		"pics_normal.zip", 
+		\%pathArchiveFolderOnly
+		);
+	saveInArchive(\@args);
 }
 
 
